fc0f9147b44bd48e8447ad62f67c5ba9648c11b8
MertArar <mertarar.ma@gmail.com>
2023-07-28 21:06:22 +0300

    testimonial created

--- src/components/Constants/index.js
+++ src/components/Constants/index.js
@@ -1,5 +1,9 @@
 import planet01 from "../Home/Explore/images/planet-01.png";
 
+import { people01 } from "../Home/Testimonials/images/people01.png";
+import { people02 } from "../Home/Testimonials/images/people02.png";
+import { people03 } from "../Home/Testimonials/images/people03.png";
+
 export const exploreWorlds = [
   {
     id: "world-1",
@@ -25,5 +29,32 @@ export const exploreWorlds = [
     id: "world-5",
     imgUrl: "../../assets/planet-05.png",
     title: "Hawkins Labs",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
   },
 ];

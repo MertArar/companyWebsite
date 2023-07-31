70e967fe0cc58eed84a0106e2d0e96735eb90cb4
MertArar <mertarar.ma@gmail.com>
2023-07-31 03:55:30 +0300

    stats img added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import Hero from "./Hero/Hero";
 import Explore from "./Explore/Explore";
 import Testimonials from "./Testimonials/Testimonials";
 import Experience from "./Experience/Experience";
+import Stats from "./Stats/Stats";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Stats />
       <Explore />
       <Testimonials />
       <Experience />

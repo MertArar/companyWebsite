5085bb1c5e3bed9666e64123415bb2b7e8f6b0fb
MertArar <mertarar.ma@gmail.com>
2023-07-31 23:33:37 +0300

    services done without responsive

--- src/components/Home/Services/Card.jsx
+++ src/components/Home/Services/Card.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-
-const Card = () => {
-  return <div>Card</div>;
-};
-
-export default Card;
+export default function Card({ children, imgSrc, ...props }) {
+  return (
+    <div
+      {...props}
+      className="relative max-w-xs overflow-hidden rounded-3xl shadow-lg group "
+    >
+      <img
+        src={imgSrc}
+        alt=""
+        className="transition-transform group-hover:scale-110 duration-200 w-[196px] h-[196px]"
+      />
+      <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/60 to-transparent">
+        <div className="p-4 text-white">{children}</div>
+      </div>
+    </div>
+  );
+}
--- src/components/Home/Services/Services.jsx
+++ src/components/Home/Services/Services.jsx
@@ -2,21 +2,41 @@ import React from "react";
 
 import Card from "./Card";
 
+import service1 from "../../../assets/services/service_1.jpeg";
+import service2 from "../../../assets/services/service_2.jpeg";
+import service3 from "../../../assets/services/service_3.jpeg";
+import service4 from "../../../assets/services/service_4.jpeg";
+
 import { BsArrowRight } from "react-icons/bs";
 
 const Services = () => {
   return (
-    <div className="flex flex-row justify-between items-center ">
+    <div className="flex flex-row justify-center gap-10 items-center ">
       {/* left side */}
-      <div className="flex flex-col gap-y-10 ">
-        <h2 className="text-white text-xl ">What Can We Do</h2>
-        <h1 className="text-white text-3xl">Services we can help you with</h1>
+      <div className="flex flex-col gap-y-6 ">
+        <h2 className="text-gray-400 text-xl ">What Can We Do</h2>
+        <h1 className="text-white text-[50px] max-w-lg font-bold">
+          Services we can help you with
+        </h1>
         <span className="text-white flex flex-row items-center gap-2 cursor-pointer">
           See All Services <BsArrowRight size={25} />{" "}
         </span>
       </div>
       {/* right side */}
-      <div className=""></div>
+      <div className="flex flex-row gap-6">
+        <Card imgSrc={service1}>
+          <h3 className="text-[22px] font-bold mb-2">UI/UX Design</h3>
+        </Card>
+        <Card imgSrc={service2}>
+          <h3 className="text-[22px] font-bold mb-2">React.js Development</h3>
+        </Card>
+        <Card imgSrc={service3}>
+          <h3 className="text-[22px] font-bold mb-2">Game Development</h3>
+        </Card>
+        <Card imgSrc={service4}>
+          <h3 className="text-[22px] font-bold mb-2">Mobile App Development</h3>
+        </Card>
+      </div>
     </div>
   );
 };

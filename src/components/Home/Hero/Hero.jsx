6cf07bc8e889ff105e94b5c8da601fb5838cf057
MertArar <85193329+MertArar@users.noreply.github.com>
2023-08-01 09:49:07 +0300

    typeWriter added in hero

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { BsArrowRight } from "react-icons/bs";
 
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
 import heroVid from "../../../assets/video3.mp4";
 
 const Hero = () => {
+  const [text] = useTypewriter({
+    words: [
+      "Website Development.",
+      "Game Development.",
+      "Mobile App Development.",
+      "UI/UX Design.",
+    ],
+    loop: true,
+    typeSpeed: 60,
+    deleteSpeed: 30,
+    delaySpeed: 2000,
+  });
   return (
     <div className="w-full h-[110vh] top-[90px] flex justify-center items-center">
       <video
@@ -14,15 +28,20 @@ const Hero = () => {
         muted
       />
       <div className="w-full h-[100%] flex flex-col ml-0 sm:ml-40 justify-center items-start text-white px-4">
-        <h1 className="text-[3rem] max-w-3xl font-bold sm:text-[6rem] text-left">
-          Creativity In Our Blood Line
+        <h1 className="text-[26px] max-w-3xl font-bold sm:text-[44px] text-left">
+          We create <br /> <span className="text-[#ff014f]">{text}</span>{" "}
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#ff014f"
+          />
         </h1>
 
         <div className="flex flex-col sm:flex-row gap-3 sm:gap-x-80 justify-between mt-5">
-          <span className="flex flex-row justify-center items-center gap-3 text-xl hover:text-red-500 hover:animate-pulse cursor-pointer">
+          <span className="flex flex-row justify-center items-center gap-3 text-[16px] sm:text-[16px] hover:text-red-500 hover:animate-pulse cursor-pointer">
             Get a Quote <BsArrowRight />
           </span>
-          <p className="text-md sm:text-xl max-w-xl py-4 text-start">
+          <p className="text-[16px] sm:text-[20px] max-w-xl py-4 text-start">
             We deliver best problem solving solution for our client and provide
             finest finishing product in present and upcoming future
           </p>

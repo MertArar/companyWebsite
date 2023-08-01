d19bcdb7bf30f30e53cc52cf5a320b53d5ca16d8
MertArar <85193329+MertArar@users.noreply.github.com>
2023-08-01 10:12:44 +0300

    titleText and typingText added to Services

--- src/components/Home/Services/Services.jsx
+++ src/components/Home/Services/Services.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import Card from "./Card";
 
+import { motion } from "framer-motion";
+
+import { TitleText, TypingText } from "./components/CustomText";
+
 import service1 from "../../../assets/services/service_1.jpeg";
 import service2 from "../../../assets/services/service_2.jpeg";
 import service3 from "../../../assets/services/service_3.jpeg";
@@ -13,15 +17,25 @@ const Services = () => {
   return (
     <div className="flex flex-col justify-center sm:flex-row ml-10 sm:ml-3 gap-10 items-center ">
       {/* left side */}
-      <div className="flex flex-col gap-y-6 ">
-        <h2 className="text-gray-400 text-xl ">What Can We Do</h2>
-        <h1 className="text-white text-[50px] max-w-lg font-bold">
-          Services we can help you with
-        </h1>
+      <motion.div
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className="flex flex-col gap-y-6 "
+      >
+        <TypingText
+          title="What Can We Do |"
+          textStyles=" text-gray-400 text-xl"
+        />
+        <TitleText
+          title={<>Services we can help you with</>}
+          textStyles="text-white text-[50px] max-w-lg font-bold"
+        />
+
         <span className="text-white flex flex-row items-center gap-2 cursor-pointer">
           See All Services <BsArrowRight size={25} />{" "}
         </span>
-      </div>
+      </motion.div>
       {/* right side */}
       <div className="flex flex-col sm:flex-row gap-6">
         <Card imgSrc={service1}>
